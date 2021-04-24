d90584136c4601ecf236bddc3b9e7b0924376c54
Surya Rekha <surya.rekha@rebtel.com>
2021-04-24 09:38:53 +0200

    dotnet chrome poc

---
+++ DockerChrome/UITest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using uitest.browser;
+
+namespace DockerChrome
+{
+    [TestClass]
+    public class UITest
+    {
+        [TestMethod]
+        public void GoogTest()
+        {
+            var driver = UiSetup.InitDriverAndOpenWebPage("chrome", "https://google.com");
+            var title = driver.Title;
+            Assert.IsNotNull(driver.Title);
+            driver.Close();
+        }
+    }
+}
---
+++ DockerChrome/Web/UiSetup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace uitest.browser
+{
+    #region Namespace Imports
+
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+
+    #endregion
+
+
+    public class UiSetup
+    {
+        public static IWebDriver getDriver { get; private set; }
+
+        public static IWebDriver InitDriver(string browser)
+        {
+            switch (browser)
+            {
+                case "chrome":
+                    getDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                    break;
+
+                case "firefox":
+                    getDriver = new FirefoxDriver();
+                    break;
+
+                default:
+                    getDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                    break;
+            }
+
+            getDriver.Manage().Window.Maximize();
+            getDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            return getDriver;
+        }
+
+        public static IWebDriver InitDriverAndOpenWebPage(string browser, string url)
+        {
+            var driver = InitDriver(browser);
+            driver.Url = url;
+            return driver;
+        }
+    }
+}
